8c6aee8552e88da1afa051061bef93b8aef7d217
AhmedSamiir51 <88041263+AhmedSamiir51@users.noreply.github.com>
2021-08-02 12:35:19 +0200

    Upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { ErrorComponent } from './Error/error/error.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { DetailsComponent } from './testing/details/details.component';
+import { AllhousesComponent } from './testing/allhouses/allhouses.component';
+import { BookingComponent } from './testing/booking/booking/booking.component';
+import { HostingComponent } from './hosting/hosting.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
+import { HomeComponent } from './page/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Auth/auth/auth.guard';
+import { HousesComponent } from './testing/houses/houses/houses.component';
+import { PersonFeedbackComponent } from './person-feedback/person-feedback.component';
+
+const routes: Routes = [
+
+  {path:"home",component:HomeComponent },
+  {path:"home/register",component:RegisterComponent},
+  {path:"home/login",component:LoginComponent},
+  {path:"Becomehost",component:HostingComponent},
+  {path:"House",component:HousesComponent},
+  {path:"Booking",component:BookingComponent},
+  {path:"House/AllHouses/:City",component:AllhousesComponent},
+  {path:"House/HousesBooking/:id",component:DetailsComponent},
+  {path:"Feedback",component:PersonFeedbackComponent},
+  {path:"",redirectTo:"home",pathMatch:"full"},
+
+  {path:'**',component:ErrorComponent},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),CarouselModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/testing/allhouses/allhouses.component.ts
@@ -0,0 +1,53 @@
+import { HousePhotoService } from './../../service/HousesPhoto/house-photo.service';
+import { UserService } from 'src/app/User/Shared/user.service';
+import { House } from './../../service/Houses/house';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HouseService } from 'src/app/service/Houses/house.service';
+
+declare var google:any
+@Component({
+  selector: 'app-allhouses',
+  templateUrl: './allhouses.component.html',
+  styleUrls: ['./allhouses.component.css']
+})
+export class AllhousesComponent implements OnInit {
+
+  constructor(public service :HouseService , private router: Router,public services :UserService,public ar:ActivatedRoute ,public serviceimg:HousePhotoService) { }
+
+  options: any;
+
+  overlays: any[]=[];
+  items:any=[]
+  itemshouse:any=[]
+  id=0
+
+  ngOnInit(): void {
+
+      this.service.getHousesByCity(this.ar.snapshot.params["City"]).subscribe(e=>{this.items=e; },er=>console.log(er))
+      this.serviceimg.GetAllHousesPhoto().subscribe(e=>{this.itemshouse=e;console.log(e)})
+
+    this.options = {
+        center: {lat: 36.890257, lng: 30.707417},
+        zoom: 12
+    };
+
+    this.overlays = [
+        new google.maps.Marker({position: {lat: 36.879466, lng: 30.667648}, title:"Giza"}),
+        new google.maps.Marker({position: {lat: 36.883707, lng: 30.689216}, title:"Ataturk Park"}),
+        new google.maps.Marker({position: {lat: 36.885233, lng: 30.702323}, title:"Oldtown"}),
+        new google.maps.Polygon({paths: [
+            {lat: 36.9177, lng: 30.7854},{lat: 36.8851, lng: 30.7802},{lat: 36.8829, lng: 30.8111},{lat: 36.9177, lng: 30.8159}
+        ], strokeOpacity: 0.5, strokeWeight: 1,fillColor: '#1976D2', fillOpacity: 0.35
+        }),
+        new google.maps.Circle({center: {lat: 36.90707, lng: 30.56533}, fillColor: '#1976D2', fillOpacity: 0.35, strokeWeight: 1, radius: 1500}),
+        new google.maps.Polyline({path: [{lat: 36.86149, lng: 30.63743},{lat: 36.86341, lng: 30.72463}], geodesic: true, strokeColor: '#FF0000', strokeOpacity: 0.5, strokeWeight: 2})
+    ];
+  }
+
+
+
+
+
+
+}
